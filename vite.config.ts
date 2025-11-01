f6fd877cbe6913601ac3a691bb0c976e1d84544d
Hrushitha12 <htigulla@charlotte.edu>
2025-11-01 01:47:02 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Hrushitha12/Portfolio', // <-- IMPORTANT for GitHub Pages
+  base: '/Portfolio/', // <-- IMPORTANT for GitHub Pages
 })
