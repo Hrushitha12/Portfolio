ea1934d482582acb8c9cb5cc75f549f0ef6248fd
Hrushitha12 <htigulla@charlotte.edu>
2025-11-01 01:18:40 -0400

    Add GH Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '', // <-- IMPORTANT for GitHub Pages
+  base: 'https://github.com/Hrushitha12/Portfolio', // <-- IMPORTANT for GitHub Pages
 })
